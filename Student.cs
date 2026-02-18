f24df0ab03df8fa7a4937aba6d62fc9456456897
Nikhil Nandanwar <NikhilNaresh.Nandanwar@cognizant.com>
2026-02-18 10:44:14 +0530

    First commit

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ODC5Application
+{
+	internal class Student
+	{
+		public string name;
+		public int age;
+		public int id;
+
+		public void PrintData()
+		{
+			Console.WriteLine($"Name: {name}");
+		}
+	}
+}
